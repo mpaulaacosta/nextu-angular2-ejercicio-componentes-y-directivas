a5492bc4583fadcafdab6ad26388720d0df1f0b4
Acosta <mpaulaacosta@outlook.com>
2019-05-23 21:14:46 -0500

    4. Crea un enrutamiento que al hacer click en el botón de cada grupo, la aplicación se dirija al componente ver-grupo.

--- tareas/src/app/app-routing.module.ts
+++ tareas/src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { NuevaTareaComponent } from './nueva-tarea/nueva-tarea.component';
+import { VerGrupoComponent } from './ver-grupo/ver-grupo.component';
 
 const routes: Routes = [
   {
     path: '', component: InicioComponent /*
     children: []*/
   },
-  { path: 'nueva-tarea', component: NuevaTareaComponent}
+  { path: 'nueva-tarea', component: NuevaTareaComponent},
+  { path: 'ver-grupo', component: VerGrupoComponent}
 ];
 
 @NgModule({
