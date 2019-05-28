6dc34f5ecd4e85733b65f6c7754e95a7fbca8140
Acosta <mpaulaacosta@outlook.com>
2019-05-27 20:25:50 -0500

    defino las tareas en el servicio

--- tareas/src/app/data.service.ts
+++ tareas/src/app/data.service.ts
@@ -10,8 +10,39 @@ export class DataService {
     {nombre: "Trabajo", resaltado: false}
   ];
 
+  private tareas: Object[] = [
+    {
+      titulo: "Recoger libros",
+      descripcion: "Ir, pedirlos y volver.",
+      fecha: "2019-05-27",
+      grupo: "Estudio"
+    },
+    {
+      titulo: "Firmar autorizaci'on",
+      descripcion: "Escribir nombre con el esfero.",
+      fecha: "2019-05-28",
+      grupo: "Trabajo"
+    },
+    {
+      titulo: "Cita con Mar'ia",
+      descripcion: "Ir a cine",
+      fecha: "2019-05-26",
+      grupo: "Familia"
+    },
+    {
+      titulo: "Estudiar Angular2",
+      descripcion: "Hacer el ejercicio",
+      fecha: "2019-05-27",
+      grupo: "Estudio"
+    }
+  ]
+
   obtenerGrupos() {
     return this.grupos;
+  }
+
+  obtenerTareas() {
+    return this.tareas;
   }
 
 }
