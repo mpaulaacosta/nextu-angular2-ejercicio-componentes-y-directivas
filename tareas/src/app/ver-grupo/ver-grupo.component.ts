47c8a8fdc67ac83ddc3260817c17ad13f866703f
Acosta <mpaulaacosta@outlook.com>
2019-05-30 21:22:04 -0500

    10. crea en el constructor un objeto de tipo ActivatedRoute, importando este módulo de @angular/route

--- tareas/src/app/ver-grupo/ver-grupo.component.ts
+++ tareas/src/app/ver-grupo/ver-grupo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 't-ver-grupo',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerGrupoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
   }
